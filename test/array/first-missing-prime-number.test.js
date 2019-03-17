66292336a8663fb016f9a0e3969f04fa45274633
yige <a@wyr.me>
2019-03-17 22:03:33 +0800

    缺失的第一个素数

--- test/array/first-missing-prime-number.test.js
+++ test/array/first-missing-prime-number.test.js
@@ -1,5 +1,6 @@
 import firstMissingPrime from '../../src/array/first-missing-prime-number'
 
 test('缺失的第一个素数', () => {
+  expect(firstMissingPrime([3, 5, 7])).toBe(2)
   expect(firstMissingPrime([2, 3, 5, 7, 11, 13, 17, 23, 29])).toBe(19)
 })
