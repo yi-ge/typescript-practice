450f1f6b5d96df43627b7b8c07518b2ec772a2da
yi-ge <a@wyr.me>
2020-07-03 15:20:06 +0800

    backup

---
+++ src/tree/convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+export const sortedArrayToBST = function (nums) {
+  if (!nums.length) return null
+
+  const creatTree = (left, right) => {
+    if (left > right) return null
+    const mid = Math.floor((left + right) / 2)
+    const root = new TreeNode(nums[mid])
+    root.left = creatTree(left, mid - 1)
+    root.right = creatTree(mid + 1, right)
+    return root
+  }
+
+  return creatTree(0, nums.length - 1)
+}
---
+++ test/tree/convert-sorted-array-to-binary-search-tree.test.js
@@ -0,0 +1,5 @@
+import { sortedArrayToBST } from '../../src/tree/convert-sorted-array-to-binary-search-tree.js'
+
+test('将有序数组转换为二叉搜索树', () => {
+  expect(sortedArrayToBST()).toEqual()
+})
