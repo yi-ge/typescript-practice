5583c89a6765b1324ce355fba02a5760512e5843
yi-ge <a@wyr.me>
2022-06-26 22:25:00 +0800

    perf: 最接近的三数之和

--- src/math/3sum-closest.ts
+++ src/math/3sum-closest.ts
@@ -2,10 +2,24 @@ export function threeSumClosest (nums: number[], target: number): number {
   nums.sort((a, b) => a - b)
   let res = nums[0] + nums[1] + nums[2]
 
-  for (let i = 0, len = nums.length - 2; i < len; i++) {
+  for (let i = 0, len = nums.length - 2; i < len && res !== target; i++) {
     let left = i + 1
     let right = len + 1
     while (left !== right) {
+      // 判断最小值
+      let min = nums[i] + nums[left] + nums[left + 1]
+      if (target < min) {
+        if (Math.abs(min - target) < Math.abs(res - target)) res = min
+        break
+      }
+
+      //判断最大值
+      let max = nums[i] + nums[right] + nums[right - 1]
+      if (target > max) {
+        if (Math.abs(max - target) < Math.abs(res - target)) res = max
+        break
+      }
+
       let sum = nums[i] + nums[left] + nums[right]
       if (Math.abs(sum - target) < Math.abs(res - target)) {
         res = sum
@@ -13,8 +27,18 @@ export function threeSumClosest (nums: number[], target: number): number {
 
       if (sum > target) {
         right--
+        while (left !== right && nums[right] === nums[right + 1]) { // 去重
+          right--
+        }
       } else {
         left++
+        while (left !== right && nums[left] === nums[left - 1]) { // 去重
+          left++
+        }
+      }
+
+      while (i < len && nums[i] == nums[i + 1]) { // 去重
+        i++
       }
     }
   }
--- test/math/3sum-closest.test.ts
+++ test/math/3sum-closest.test.ts
@@ -3,4 +3,7 @@ import { threeSumClosest } from '../../src/math/3sum-closest'
 test('最接近的三数之和', () => {
   expect(threeSumClosest([-1, 2, 1, -4], 1)).toBe(2)
   expect(threeSumClosest([0, 0, 0], 1)).toBe(0)
+  expect(threeSumClosest([1, 8, 5, 7, 2, 1], 1)).toBe(4)
+  expect(threeSumClosest([1, 1, -1, -1, 3], 3)).toBe(3)
+  expect(threeSumClosest([-1, 0, 1, 1, 55], 3)).toBe(2)
 })
