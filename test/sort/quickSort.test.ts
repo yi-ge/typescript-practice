f9e01d310fd8625cc043d1877d39492b11644791
lishifu666 <1522120777@qq.com>
2022-06-27 17:57:10 +0800

    算法补充修改

--- test/sort/quickSort.test.ts
+++ test/sort/quickSort.test.ts
@@ -2,8 +2,8 @@ import { quickSort } from '../../src/sort/quickSort'
 import { mergeSort } from '../../src/sort/mergeSort'
 
 test('快排', () => {
-    let arr = [2, 6, 1, 4, 8, 4, 7, 8, 97, 3, 2]
-    quickSort(arr)
+    let arr = [2, 6, 1, 4, 8, 4, 7, 8, 97, 3, 2];
+    quickSort(arr);
     expect(arr).toEqual(mergeSort([2, 6, 1, 4, 8, 4, 7, 8, 97, 3, 2]))
     
     const data = [
@@ -15,7 +15,7 @@ test('快排', () => {
 
     for (let i = 0; i < data.length; i++) {
       let arr = data[i];
-      quickSort(arr)
+      quickSort(arr);
         expect(arr).toEqual(mergeSort(JSON.parse(JSON.stringify(data[i]))))
     }
 })
