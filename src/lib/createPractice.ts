320a98d4209433f097444d83c78498193c927480
yi-ge <a@wyr.me>
2022-06-28 22:49:57 +0800

    feat: add ts-node

---
+++ src/lib/createPractice.ts
@@ -0,0 +1,23 @@
+import { argv, exit } from 'process'
+import puppeteer from 'puppeteer'
+
+if (!argv[2]) {
+  console.log('请在命令后添加LeetCode URL。')
+  exit(0)
+}
+
+const url = argv[2]
+
+// fetch(url)
+//   .then(res => res.text())
+//   .then(text => console.log(text))
+
+const browser = await puppeteer.launch({
+  headless: false
+})
+const page = await browser.newPage()
+await page.goto(url)
+await page.waitForTimeout(5000)
+await page.screenshot({ path: 'example.png' })
+await browser.close()
+
