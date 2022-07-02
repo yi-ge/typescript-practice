84242ec37feb4339985079ef32d2a54814ddcfac
yi-ge <a@wyr.me>
2022-07-02 13:52:15 +0800

    add:  最低加油次数

---
+++ src/other/minimum-number-of-refueling-stops.ts
@@ -0,0 +1,6 @@
+// 最低加油次数
+// https://leetcode.cn/problems/minimum-number-of-refueling-stops
+
+export function minRefuelStops (target: number, startFuel: number, stations: number[][]): number {
+  return 0
+}
