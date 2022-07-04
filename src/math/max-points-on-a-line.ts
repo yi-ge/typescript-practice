deeaefeba09e3cdf3dbb78ad892de8e2c5a980c5
yi-ge <a@wyr.me>
2022-07-04 11:23:15 +0800

    perf: 直线上最多的点数

--- src/math/max-points-on-a-line.ts
+++ src/math/max-points-on-a-line.ts
@@ -3,32 +3,41 @@
 // INLINE  ../../images/math/max-points-on-a-line.jpeg
 
 export function maxPoints (points: number[][]): number {
-  let maxPoint = 0
+  let maxPoint = 1
 
   const GCD = (a: number, b: number): number => b ? GCD(b, a % b) : a
   const k = (a: number[], b: number[]): [number, number] => {
-    const numerator = b[1] - a[1]
-    const denominator = b[0] - a[0]
-    const gcd = GCD(numerator, denominator)
-    return [numerator / gcd, denominator / gcd]
+    let numerator = b[1] - a[1] // ?
+    let denominator = b[0] - a[0] // ?
+    const gcd = GCD(Math.abs(numerator), Math.abs(denominator)) /* ? */ // 取最大公约数
+    numerator = numerator / gcd /* ? */
+    denominator = denominator / gcd /* ? */
+    if (numerator < 0 && denominator < 0) { // 如果分子分母都是负数
+      numerator = Math.abs(numerator)
+      denominator = Math.abs(denominator)
+    } else if (numerator > 0 && denominator < 0) { // 如果分母是负数，转移到分子
+      denominator = Math.abs(denominator)
+      numerator = -numerator
+    }
+    return [numerator, denominator] // ?
   }
 
   for (let i = 0; i < points.length; i++) {
-    const map = new Map<[number, number], number>()
+    const map = new Map<string, number>()
     for (let j = 0; j < points.length; j++) {
       if (i !== j) {
-        const d = k(points[i], points[j])
-        const tmp = map.get(d)
+        const d = k(points[i], points[j]).join(',') // ?
+        const tmp = map.get(d)  // ?
         if (tmp) {
-          map.set(d, tmp + 1)
+          map.set(d, tmp + 1) // ?
         } else {
           map.set(d, 1)
         }
       }
     }
 
-    map.forEach(item => {
-      maxPoint = Math.max(maxPoint, item)
+    map.forEach(value => {
+      maxPoint = Math.max(maxPoint, value + 1 /* ? */)
     })
   }
 
--- test/math/max-points-on-a-line.test.ts
+++ test/math/max-points-on-a-line.test.ts
@@ -3,4 +3,5 @@ import { maxPoints } from '../../src/math/max-points-on-a-line'
 test('直线上最多的点数', () => {
   expect(maxPoints([[1, 1], [2, 2], [3, 3]])).toBe(3)
   expect(maxPoints([[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]])).toBe(4)
+  expect(maxPoints([[0, 0]])).toBe(1)
 })
