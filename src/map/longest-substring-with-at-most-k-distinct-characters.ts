f1c8791601f79d364a0c9c1825906ac76d36fb6f
yi-ge <a@wyr.me>
2022-09-20 19:57:32 +0800

    fix: 至多包含 K 个不同字符的最长子串

--- src/map/longest-substring-with-at-most-k-distinct-characters.ts
+++ src/map/longest-substring-with-at-most-k-distinct-characters.ts
@@ -7,14 +7,14 @@ export function lengthOfLongestSubstringKDistinct (s: string, k: number): number
   let left = 0, right = 0 // 左右边界
   const map = new Map<string, number>() // 记录在k范围内每个字符出现的次数
   let ans = 0
-  while (right < s.length) {
+  while (right <= s.length) {
     if (map.size <= k) {
       if (map.has(s[right])) {
         map.set(s[right], map.get(s[right]) as number + 1)
       } else {
         map.set(s[right], 1)
       }
-      ans = Math.max(ans, right - left)
+      ans = Math.max(ans, right - left) // ?
       right++
     } else { // map.size > k
       const count = map.get(s[left]) as number - 1
